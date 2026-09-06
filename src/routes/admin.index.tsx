49949d37ef6fd287ce0bdfdf55c3cadaa39b2e36
abdulbasitofficial1 <asifabdulbasit7@gmail.com>
2026-09-06 17:18:09 +0500

    Update admin.index.tsx

--- src/routes/admin.index.tsx
+++ src/routes/admin.index.tsx
@@ -20,8 +20,8 @@ import {
 } from "lucide-react";
 
 import { ImportProductsDialog } from "@/components/nexas/admin/ImportProductsDialog";
-import { BulkAddProductsDialog } from "@/components/admin/BulkAddProductsDialog";
-import { RepairImagesDialog } from "@/components/admin/RepairImagesDialog";
+import { BulkAddProductsDialog } from "@/components/nexas/admin/BulkAddProductsDialog";
+import { RepairImagesDialog } from "@/components/nexas/admin/RepairImagesDialog";;
 import { ChatsPanel } from "@/components/nexas/admin/ChatsPanel";
 
 import { Button } from "@/components/ui/button";
